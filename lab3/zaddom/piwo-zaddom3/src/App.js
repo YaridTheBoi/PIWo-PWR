fc6e637c3bb940ad1c89c02f1450167ee384f9ad
Casper Chrostowski <kacper.chrostowski@gmail.com>
2023-05-21 20:53:23 +0200

    login zrobiony

--- lab3/zaddom/piwo-zaddom3/src/App.js
+++ lab3/zaddom/piwo-zaddom3/src/App.js
@@ -1,15 +1,19 @@
 import {BrowserRouter, Link, Route, Routes} from 'react-router-dom'
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import Main from './Pages/Main';
 import BookMeeting from './Pages/BookMeeting';
 import AddNew from './Pages/AddNew';
 import Login from './Pages/Login';
 import './App.css';
+import UserContext from './Data/UserContext';
 
 function App() {
 
 
   const [apartmentData, setApartmentData] = useState([]);
+  const [user, setUser] = useState("");
+
+
   useEffect(()=>{
 
     const fetchOptions = {
@@ -18,43 +22,48 @@ function App() {
         Accept : "application/json",
         "Content-Type" : "application/json;charset=UTF-8",
       }
-    }
+    };
     const promise = fetch("./data/apartments.json", fetchOptions);
     promise.then(response => response.json()).then(content => {
       setApartmentData(content);
-    })
-  }, [])
+    });
+  }, []);
 
+  const logout = () =>{
+    setUser("");
+  };
 
   console.log(apartmentData);
+
   let mainJSX = apartmentData.length && <Main apartmentData={apartmentData} />;
   return ( 
     <div>
-      <BrowserRouter>
-        <nav>
-          <Link to='/' className="navbar-brand nav-link">RentHouse</Link>
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link to='/bookmeeting' className="nav-link">Book Meeting</Link>
-            </li>
-            <li className="nav-item">
-              <Link to='/addnew' className="nav-link">Add New</Link>
-            </li>
-            <li className="nav-item">
-              <Link to='/login' className="nav-link">Login</Link>
-            </li>
-          </ul>
-        </nav>
+      <UserContext.Provider value={{user, setUser}}>
+        
+        <BrowserRouter>
+          <nav>
+            <Link to='/' className="navbar-brand nav-link">RentHouse</Link>
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link to='/bookmeeting' className="nav-link">Book Meeting</Link>
+              </li>
+              <li className="nav-item">
+                <Link to='/addnew' className="nav-link">Add New</Link>
+              </li>
+              <li className="nav-item">
+                {user ? <b>Hello {user} <button onClick={logout}>Logout</button></b>: <Link to='/login' className="nav-link">Login</Link>}
+              </li>
+            </ul>
+          </nav>
 
-        <Routes>
-          <Route path='/' element={mainJSX}/>
-          <Route path='/bookmeeting' element={<BookMeeting></BookMeeting>} />
-          <Route path='/addnew' element={<AddNew setApartmentData={setApartmentData}></AddNew>} />
-          <Route path='/login' element={<Login></Login>} />
-        </Routes>
-      </BrowserRouter>
-
-      
+          <Routes>
+            <Route path='/' element={mainJSX}/>
+            <Route path='/bookmeeting' element={<BookMeeting></BookMeeting>} />
+            <Route path='/addnew' element={<AddNew setApartmentData={setApartmentData}></AddNew>} />
+            <Route path='/login' element={<Login></Login>} />
+          </Routes>
+        </BrowserRouter>
+      </UserContext.Provider>
     </div>
    
   );
--- lab3/zaddom/piwo-zaddom3/src/Pages/Login.jsx
+++ lab3/zaddom/piwo-zaddom3/src/Pages/Login.jsx
@@ -1,32 +1,60 @@
-import { useState, useEffect } from 'react';
-
+import { useState, useEffect, useContext } from 'react';
+import UserContext from '../Data/UserContext';
+import { useNavigate } from 'react-router-dom';
 const Login = () => {
 
+
+    const navigate = useNavigate();
+
     const [usersData, setUsersData] = useState([]);
+    const [emailLogin, setEmailLogin] = useState("");
+    const [passwordLogin, setPasswordLogin] = useState("");
+
+    const { setUser } = useContext(UserContext);
 
 
-    useEffect(() => {
-      const fetchUsers = async () => {
-        try {
-          const response = await fetch('./data/users.json');
-          const jsonData = await response.json();
-          setUsersData(jsonData.map(it => it.text))
-        } catch (error) {
-          console.log('Błąd ładowania json:', error);
+    useEffect(()=>{
+
+      const fetchOptions = {
+        method: "GET",
+        headers : {
+          Accept : "application/json",
+          "Content-Type" : "application/json;charset=UTF-8",
         }
       };
-  
-      fetchUsers();
+
+      const promise = fetch("./data/users.json", fetchOptions);
+      promise.then(response => response.json()).then(content => {
+        setUsersData(content);
+      });
     }, []);
 
+
+    const handleLogin = () =>{
+      const foundUser = usersData.find(user => user.mail === emailLogin && user.password === passwordLogin);
+
+      if (foundUser){
+        console.log("Poprawnie zalogowano")
+        setUser(foundUser.name)
+        setEmailLogin("")
+        setPasswordLogin("")
+        navigate('/')
+      }else{
+        console.log("Niepoprawne dane logowania")
+      }
+    };
+
+
+  
+    //console.log(user)
     return(
         <div>
 
-          <input placeholder='Login' ></input>
+          <input placeholder='Email' value = {emailLogin} onChange={e => setEmailLogin(e.target.value) }></input>
 
-          <input placeholder='Haslo' type="password" ></input>
-
-          <button>Login</button>
+          <input placeholder='Haslo' value= {passwordLogin} onChange={e => setPasswordLogin(e.target.value)} type="password" ></input>
+ 
+          <button onClick={handleLogin}>Login</button>
 
         </div>
     );
